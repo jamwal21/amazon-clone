717d11f3c4eb38f55b59bb0f750cf63a18df72e1
Abhinav Jamwal <abhinavjamwal302@gmail.com>
2020-09-14 23:23:35 +0530

    added remove item from cart action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,4 +3,34 @@ export const initialState = {
     user: null
 };
 
-const reducer = ()
+//Selector
+export const cartTotal = (basket) => (
+    basket?.reduce(( amount, item ) => item.price + amount, 0)
+)
+
+const reducer = (state, action) => {
+    console.log(action);
+    
+    switch(action.type){
+        case "ADD_TO_BASKET" : 
+            return {
+                ...state,
+                basket: [...state.basket,action.item]
+            };
+
+        case "REMOVE_FROM_BASKET" :
+            
+            var itemRemoving = state.basket.findIndex( item => item.id === action.id)
+            console.log(itemRemoving);
+            var updatedState = [...state.basket]
+            updatedState.splice(itemRemoving,1)
+            return{
+                ...state,
+                basket: updatedState
+            }
+            
+        default : return initialState;
+    }
+}
+
+export default reducer
